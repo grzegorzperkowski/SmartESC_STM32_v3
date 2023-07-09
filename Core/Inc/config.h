f3c57adbe7c7422963e22ebe1a6c309812e25933
grzegorzperkowski <gperkowski@gmail.com>
2023-07-09 09:55:35 +0200

    Update config.h
    Set max speed to 25/40 km/h

--- Core/Inc/config.h
+++ Core/Inc/config.h
@@ -5,8 +5,8 @@
 
 // speed limits for invividual modes in kph
 #define SPEEDLIMIT_ECO 6
-#define SPEEDLIMIT_NORMAL 20
-#define SPEEDLIMIT_SPORT 50
+#define SPEEDLIMIT_NORMAL 25
+#define SPEEDLIMIT_SPORT 40
 
 // motor current limits for invividual modes in mA
 // note that hacked firmware allows up to 55amps motor phase current
